b7472d02ef7d3d6d0aef6220cd101052db9a5fa2
Shridhar TL <shridhar.tl@gmail.com>
2026-02-01 14:32:32 +0530

    Latest package deployed

--- assets/MediaManager-BQq2V0S0.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"../index.ChXpIm8i.js";import{M as r}from"./MediaGrid-BNp8TO_l.js";const t=()=>e.jsxs("div",{className:"space-y-6 p-6",children:[e.jsx("h1",{className:"text-2xl font-bold",children:"Media Manager"}),e.jsx(r,{showUpload:!0,showViewToggle:!0,showSearch:!0,showFilter:!0})]});export{t as default};
-//# sourceMappingURL=MediaManager-BQq2V0S0.js.map
