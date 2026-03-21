0140f5171307abff675f6b0a4e08e2f789e893c4
Aishwarya S <aishwaryakm222@gmail.com>
2026-03-21 14:11:44 +0530

    Updated frontend build output

--- assets/MediaManager-mUJSYfG4.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"../index.cErsZgLa.js";import{M as r}from"./MediaGrid-B3JK0WNx.js";const t=()=>e.jsxs("div",{className:"space-y-6 p-6",children:[e.jsx("h1",{className:"text-2xl font-bold",children:"Media Manager"}),e.jsx(r,{showUpload:!0,showViewToggle:!0,showSearch:!0,showFilter:!0})]});export{t as default};
